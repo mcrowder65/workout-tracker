b6d7257921503d2a9411c76812a5676ecf186366
Matt Crowder <mcrowder65@gmail.com>
2019-02-02 16:28:16 -0500

    added some husky

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,13 +1,37 @@
 import React from "react";
+import compose from "lodash.compose";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
 
+import BottomNavigation from "./bottom-navigation";
 import Workout from "./workout";
 
-function App() {
+function App({ classes }) {
   return (
     <React.Fragment>
-      <Workout />
+      <div>
+        <div className={classes.centered}>
+          <Workout />
+        </div>
+        <footer>
+          <BottomNavigation />
+        </footer>
+      </div>
     </React.Fragment>
   );
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+const styles = {
+  body: {},
+  centered: {
+    display: "flex",
+    justifyContent: "center",
+  },
+};
+
+const enhance = compose(withStyles(styles));
+export default enhance(App);
---
+++ src/client/components/bottom-navigation.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  BottomNavigation as MuiBottomNavigation,
+  BottomNavigationAction
+} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+import Restore from "@material-ui/icons/Restore";
+
+import LocationOn from "@material-ui/icons/LocationOn";
+
+import Favorite from "@material-ui/icons/Favorite";
+import compose from "lodash.compose";
+
+function BottomNavigation(props) {
+  return (
+    <MuiBottomNavigation>
+      <BottomNavigationAction label="Recents" icon={<Restore />} />
+      <BottomNavigationAction label="Favorites" icon={<Favorite />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOn />} />
+    </MuiBottomNavigation>
+  );
+}
+
+BottomNavigation.propTypes = {};
+
+const styles = {};
+
+const enhance = compose(withStyles(styles));
+export default enhance(BottomNavigation);
